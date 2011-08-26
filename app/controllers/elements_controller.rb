251392bd8bee815ee49027fd21801963c1f06bc2
Heiser Mazariegos <hmazariegos@inside-solutions.net>
2011-08-26 12:34:41 -0600

    cambios de heiser

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -25,7 +25,7 @@ class ElementsController < AclController
   # GET /elements/new.xml
   def new
     @element = Element.new
-    @root_category = Category.find(272)
+    @root_category = Category.find(286)
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @element }
@@ -41,15 +41,48 @@ class ElementsController < AclController
   # POST /elements
   # POST /elements.xml
   def create
-    @element = Element.new(params[:element])
+    #@element = Element.new(params[:element])
+
+    mca_hash = params[:element]
+    mca_cats = mca_hash[:category_id].split(',')
+    #mca_secondcats = mca_hash[:second_category_id].split(',')
+    errors = []
+    mca_cats.each do |c|
+      unless c.blank?
+        c = c.to_i
+        mca_hash_temp = mca_hash
+        mca_hash_temp[:category_id] = c
+        #mca_hash_temp[:second_category_id] = mca_secondcats.last
+        @element = Element.new(mca_hash_temp)
+        begin
+          @element.save
+        rescue ActiveRecord::StatementInvalid
+          # ignore duplicate issues. how to add ignore parameter to sql query here without changing to sql completely?
+        else
+          errors.push( @element.errors )
+        end
+      end
+    end
+
+
 
     respond_to do |format|
-      if @element.save
+      #if @element.save
+      #  flash[:notice] = 'Element was successfully created.'
+      #  format.html { redirect_to(@element) }
+      #  format.xml  { render :xml => @element, :status => :created, :location => @element }
+      #else
+      #  @root_category = Category.find(272)
+      #  format.html { render :action => "new" }
+      #  format.xml  { render :xml => @element.errors, :status => :unprocessable_entity }
+      #end
+      unless errors.length > 0
         flash[:notice] = 'Element was successfully created.'
         format.html { redirect_to(@element) }
         format.xml  { render :xml => @element, :status => :created, :location => @element }
       else
         @root_category = Category.find(272)
+        flash[:notice] = errors.join(', ')
         format.html { render :action => "new" }
         format.xml  { render :xml => @element.errors, :status => :unprocessable_entity }
       end
--- app/models/element.rb
+++ app/models/element.rb
@@ -1,4 +1,4 @@
 class Element < ActiveRecord::Base
-  belongs_to :category
+  belongs_to :category, :class_name => 'Category'
   belongs_to :second_category, :class_name => 'Category'
 end
