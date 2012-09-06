111143dbaa6f0dc6933fd6b4571aad3e22aaad04
Andres Montano <amontano@virginia.edu>
2012-09-06 13:45:03 -0600

    updating rails 3.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KmapsSample::Application.config.secret_token = '9a1f25e03f28c6807b8901e367a4f17e73f52627d96acf2910d3dd343cea0b6117b46368e75cf6c75910027eed4b470ab49d16d55a5a7cc61cfa3d803e5d8c79'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_kmaps_sample_session',
-  :secret      => '6acc0e30990189dc6661fbe7193984c173b36bcdccd09cb083ae975f5f3fcd98113cc8539960ea9dd5c0511f7f749fc746201c0e20c689ebc53bdd094f68e75c'
-}
+KmapsSample::Application.config.session_store :cookie_store, :key => '_kmaps_sample_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# KmapsSample::Application.config.session_store :active_record_store
